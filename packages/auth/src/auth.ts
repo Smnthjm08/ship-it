fae39561f3b54d7c0fa66d11b6e8a33a40d0feee
Smnthjm08 <sumanthjmm.04@gmail.com>
2026-01-07 23:58:05 +0530

    feat: added auth package and github oauth

---
+++ packages/auth/src/auth.ts
@@ -0,0 +1,18 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "@repo/db";
+
+// console.log("db in auth.ts", db);
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql", // or "mysql", "sqlite"
+  }),
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+      scope: ["repo"],
+    },
+  },
+});
